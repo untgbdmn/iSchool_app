44c463c9995c388ecce3fcf53612621da0a5ada1
untgbdmn <untungbudiman629@gmail.com>
2024-12-23 21:04:28 +0700

    temps

--- layouts/guest-layout.tsx
+++ layouts/guest-layout.tsx
@@ -1,18 +1,24 @@
 import Image from 'next/image'
 import React from 'react'
 import Logo from '../assets/icon/logo.svg'
+import Link from 'next/link'
 
 interface GuestLayoutProps {
-    children: React.ReactNode
+    children?: React.ReactNode
 }
 
 export default function GuestLayout({children}:GuestLayoutProps ) {
   return (
     <div>
         <div className="">
-            <div className="card">
-                <div className="">
+            <div className="card flex items-center justify-between">
+                <div className="flex items-center">
                     <Image src={Logo} alt='Logo' className='size-12'/>
+                    <p className="text-2xl font-sans font-bold logo-text">iSiman</p>
+                </div>
+                <div className="flex flex-row items-center gap-5">
+                    <Link href="/login" className='px-5 bg-violet-600 text-white font-semibold py-2 rounded-md'>Login</Link>
+                    <Link href="/login" className='px-5 rounded-md border-violet-700 py-2 border-2 text-violet-700'>Register</Link>
                 </div>
             </div>
         </div>
